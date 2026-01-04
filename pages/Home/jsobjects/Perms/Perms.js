fff4962a096bb1ced2bc1811dc96dca9323861b5
Caue Figueiredo <caue.figueiredo@gmail.com>
2026-01-04 19:45:09 +0000

    commit to master

--- pages/Home/jsobjects/Perms/Perms.js
+++ pages/Home/jsobjects/Perms/Perms.js
@@ -1,27 +1,18 @@
 export default {
   init: async () => {
-    const email = String(appsmith.user.email || '').trim().toLowerCase();
+    if (appsmith.store.pagePermsInited) return;
 
-    // rerun only if different user or not initialized
-    if (appsmith.store.myPerms?.inited && appsmith.store.myPerms?.email === email) return;
-
-    await get_my_role.run();
-    await get_my_permissions.run();
     await get_my_pages.run();
 
-    const permKeys = (get_my_permissions.data || []).map(x => x.permission_key).filter(Boolean);
-    const pages = (get_my_pages.data || []).map(x => x.page_value).filter(Boolean);
+    const pages = (get_my_pages.data || [])
+      .map(x => x.page_value)
+      .filter(Boolean);
 
-    await storeValue("myPerms", { inited: true, email, permKeys, pages });
+    await storeValue('pagePerms', pages);
+    await storeValue('pagePermsInited', true);
   },
 
-  canPage: (pageName) => {
-    return (appsmith.store.myPerms?.pages || []).includes(pageName);
-  },
-
-  // IMPORTANT: use Perms.canPage (NOT this.canPage)
-  canMenu: (menuLabel) => {
-    const items = AppNav.itemsForLabel(menuLabel) || [];
-    return items.some(it => Perms.canPage(it.value));
+  canAccessPage: (pageName) => {
+    return (appsmith.store.pagePerms || []).includes(pageName);
   }
 };
